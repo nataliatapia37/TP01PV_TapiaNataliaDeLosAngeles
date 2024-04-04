89efe647f297f15f8fca62c0a0239ed08dbddda4
Tapia Natalia <Tapia Natalia@DESKTOP-7Q80SBM>
2024-04-04 15:41:22 -0300

    Ejercicio 1 terminado

---
+++ src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int numero01 = 8;
+		int numero02 = 5;
+		int numero03 = 1;
+		int numero04 = 20;
+		int numero05 = 9;
+		
+		double promedio = (double)(numero01 + numero02 + numero03 + numero04 + numero05)/ 5;
+		
+		System.out.println("El promedio es: " + promedio);
+		
+	}
+
+}
